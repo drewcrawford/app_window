2c78e7fe0b7524d64db9f00e21ed02438f835dc8
Drew Crawford <drew@sealedabstract.com>
2025-01-07 17:58:23 -0600

    Support for creating windows

--- src/sys/windows.rs
+++ src/sys/windows.rs
@@ -2,9 +2,14 @@ use std::ffi::c_void;
 use std::fmt::Display;
 use std::sync::Arc;
 use raw_window_handle::{RawDisplayHandle, RawWindowHandle};
-use windows::Win32::Foundation::HWND;
-use windows::Win32::UI::WindowsAndMessaging::{DispatchMessageW, GetMessageW, TranslateMessage, MSG};
+use send_cells::send_cell::SendCell;
+use windows::core::{w, HSTRING, PCWSTR};
+use windows::Win32::Foundation::{GetLastError, HINSTANCE, HSTR, HWND, LPARAM, LRESULT, WPARAM};
+use windows::Win32::Graphics::Gdi::{COLOR_WINDOW, HBRUSH};
+use windows::Win32::System::LibraryLoader::GetModuleHandleW;
+use windows::Win32::UI::WindowsAndMessaging::{CreateWindowExW, DefWindowProcW, DispatchMessageW, GetMessageW, LoadCursorW, PeekMessageW, PostThreadMessageW, RegisterClassExW, ShowWindow, TranslateMessage, HMENU, IDC_ARROW, MSG, PM_NOREMOVE, SW_SHOWNORMAL, WINDOW_EX_STYLE, WM_USER, WNDCLASSEXW, WS_OVERLAPPEDWINDOW};
 use crate::coordinates::{Position, Size};
+const WM_RUN_FUNCTION: u32 = WM_USER;
 
 #[derive(Debug)]
 pub struct FullscreenError;
@@ -39,46 +44,119 @@ pub fn is_main_thread() -> bool {
     current_id == main_thread_id()
 }
 
+struct WinClosure(Box<dyn FnOnce() + Send + 'static>);
+
+
 
 pub fn run_main_thread<F: FnOnce() -> () + Send + 'static>(closure: F) {
-    closure(); //I think it's ok to run inline on windows?
+    //need to create a message queue first
     let mut message = MSG::default();
+    _ = unsafe{PeekMessageW(&mut message, HWND::default(), WM_USER, WM_USER, PM_NOREMOVE)}; //create a message queue
+    //we don't care about the return value of PeekMessageW, it simply tells us if messages are available or not
+
+    //now the queue is available so subsequent calls to PostMessageW will work
+    closure(); //I think it's ok to run inline on windows?
     let all_hwnd = HWND::default();
     loop {
         let message_ret = unsafe{GetMessageW(&mut message, all_hwnd, 0, 0)};
+        println!("got message {:?}",message);
         if message_ret.0 == 0 {
             break;
         }
         else if message_ret.0 == -1 {
             panic!("GetMessageW failed");
         }
-        unsafe {
-            //ms code seems to ignore this return value in practice
-            //see https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getmessage
-            _ = TranslateMessage(&mut message);
-            DispatchMessageW(&mut message);
+        match message.message {
+            WM_RUN_FUNCTION => {
+                let as_usize = message.wParam.0;
+                let winclosure = unsafe{Box::from_raw(as_usize as *mut WinClosure)};
+                winclosure.0();
+            }
+            _ => {
+                unsafe {
+                    //ms code seems to ignore this return value in practice
+                    //see https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getmessage
+                    _ = TranslateMessage(&mut message);
+                    DispatchMessageW(&mut message);
+                }
+            }
         }
+
     }
 }
 
-pub fn on_main_thread<F: FnOnce()>(_closure: F) {
-    todo!()
+pub fn on_main_thread<F: FnOnce() + Send + 'static>(closure: F) {
+    let boxed_closure = Box::new(WinClosure(Box::new(closure)));
+    let closure_ptr = Box::into_raw(boxed_closure) as *mut ();
+    let as_usize = closure_ptr as usize;
+    unsafe { PostThreadMessageW(main_thread_id(), WM_RUN_FUNCTION, WPARAM(as_usize), LPARAM(0)) }.expect("PostThreadMessageW failed");
 }
 
 pub struct Window {
-    imp: *mut c_void,
+    hwnd: SendCell<HWND>,
 }
 
 unsafe impl Send for Window {}
 unsafe impl Sync for Window {}
 
+extern "system" fn window_proc(hwnd: HWND, msg: u32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
+    eprintln!("got msg hwnd {hwnd:?} msg {msg} w_param {w_param:?} l_param {l_param:?}");
+    match msg {
+        _ => {
+            unsafe{DefWindowProcW(hwnd,msg,w_param, l_param)}
+        }
+    }
+}
+
 impl Window {
-    pub async fn new(_position: Position, _size: Size, _title: String) -> Self {
-        todo!()
+    pub async fn new(position: Position, size: Size, title: String) -> Self {
+        let window = crate::application::on_main_thread(move || {
+
+            let instance = unsafe{GetModuleHandleW(PCWSTR::null())}.expect("Can't get module");
+            let cursor = unsafe{LoadCursorW(HINSTANCE::default(), IDC_ARROW)}.expect("Can't load cursor");
+            let winstr: HSTRING = title.into();
+            let class_name = w!("raw_input_debug_window");
+            let window_class = WNDCLASSEXW {
+                cbSize: std::mem::size_of::<WNDCLASSEXW>() as u32,
+                style: Default::default(),
+                lpfnWndProc: Some(window_proc),
+                cbClsExtra: 0,
+                cbWndExtra: 0,
+                hInstance: instance.into(),
+                hIcon: Default::default(),
+                hCursor: cursor,
+                hbrBackground: HBRUSH(COLOR_WINDOW.0 as usize as *mut c_void),
+                lpszMenuName: PCWSTR::null(),
+                lpszClassName: class_name,
+                hIconSm: Default::default(),
+            };
+            let r = unsafe{RegisterClassExW(&window_class)};
+            assert_ne!(r, 0, "failed to register window class: {:?}",unsafe{GetLastError()});
+
+            let window = unsafe{CreateWindowExW(WINDOW_EX_STYLE(0), //style
+                                   class_name,
+                                   &winstr,
+                                   WS_OVERLAPPEDWINDOW,
+                                   position.x() as i32, position.y() as i32, //position
+                                   size.width() as i32, size.height() as i32, //size
+                                   HWND(std::ptr::null_mut()), //parent
+                                   HMENU(std::ptr::null_mut()), //menu
+                                   instance, //instance
+                                   None,
+
+            )}.expect("failed to create window");
+            unsafe{_ = ShowWindow(window, SW_SHOWNORMAL)};
+            SendCell::new(window)
+        }).await;
+
+        Window {
+            hwnd: window,
+        }
+
     }
 
     pub async fn default() -> Self {
-        todo!()
+        Self::new(Position::new(0.0, 0.0), Size::new(800.0, 600.0), "app_window".to_string()).await
     }
 
     pub async fn fullscreen(_title: String) -> Result<Self, FullscreenError> {
