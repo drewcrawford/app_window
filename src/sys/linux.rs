52f2bcf201658746e58d310573538b5de5a14bf0
Drew Crawford <drew@sealedabstract.com>
2025-01-08 19:07:05 -0600

    Sketch window creation

--- src/sys/linux.rs
+++ src/sys/linux.rs
@@ -1,4 +1,24 @@
+use std::cell::RefCell;
+use std::fs::File;
+use std::os::fd::AsFd;
+use std::sync::mpsc::{channel, Sender};
+use std::sync::OnceLock;
+use libc::{getpid, pid_t, syscall, SYS_gettid};
+use memmap2::MmapMut;
 use raw_window_handle::{RawDisplayHandle, RawWindowHandle};
+use wayland_client::{Connection, Dispatch, Proxy, QueueHandle};
+use wayland_client::globals::{registry_queue_init, GlobalListContents};
+use wayland_client::protocol::{wl_compositor, wl_registry, wl_shm};
+use wayland_client::protocol::wl_buffer::WlBuffer;
+use wayland_client::protocol::wl_compositor::WlCompositor;
+use wayland_client::protocol::wl_registry::WlRegistry;
+use wayland_client::protocol::wl_seat::WlSeat;
+use wayland_client::protocol::wl_shm::{Format, WlShm};
+use wayland_client::protocol::wl_shm_pool::WlShmPool;
+use wayland_client::protocol::wl_surface::WlSurface;
+use wayland_protocols::xdg::shell::client::xdg_surface::XdgSurface;
+use wayland_protocols::xdg::shell::client::xdg_toplevel::XdgToplevel;
+use wayland_protocols::xdg::shell::client::xdg_wm_base::XdgWmBase;
 use crate::coordinates::{Position, Size};
 
 #[derive(Debug)]
@@ -13,11 +33,22 @@ impl std::fmt::Display for FullscreenError {
 }
 
 pub fn is_main_thread() -> bool {
-    todo!()
+    let current_pid = unsafe{getpid()};
+    let main_thread_pid = unsafe{syscall(SYS_gettid)} as pid_t;
+    current_pid == main_thread_pid
 }
 
-pub fn run_main_thread<F: FnOnce() -> () + Send + 'static>(_closure: F) {
-    todo!()
+static MAIN_THREAD_SENDER: OnceLock<Sender<Box<dyn FnOnce() + Send>>> = OnceLock::new();
+
+pub fn run_main_thread<F: FnOnce() -> () + Send + 'static>(closure: F) {
+    closure();
+    //park
+    let (sender,receiver) = channel();
+    MAIN_THREAD_SENDER.get_or_init(|| sender);
+    loop {
+        let msg = receiver.recv().expect("Main thread receiver closed");
+        msg();
+    }
 }
 
 pub fn on_main_thread<F: FnOnce()>(_closure: F) {
@@ -31,13 +62,156 @@ pub struct Window {
 unsafe impl Send for Window {}
 unsafe impl Sync for Window {}
 
+struct App;
+
+fn create_shm_buffer(
+    _shm: &wl_shm::WlShm,
+    width: u32,
+    height: u32,
+) -> (File, MmapMut) {
+    let stride = width * 4;
+    let size = stride * height;
+    let file = tempfile::tempfile().unwrap();
+    file.set_len(size as u64).unwrap();
+
+    let mut mmap = unsafe{MmapMut::map_mut(&file)}.unwrap();
+
+    for pixel in mmap.chunks_exact_mut(4) {
+        pixel.copy_from_slice(&[0, 0, 0xFF, 0xFF]); //I guess due to endiannness we are actually BGRA?
+    }
+
+    (file, mmap)
+}
+
+
+impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for App {
+    fn event(
+        _state: &mut Self,
+        _registry: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        _: &GlobalListContents,
+        _: &Connection,
+        _qh: &QueueHandle<App>,
+    ) {
+        println!("Got registry event {:?}",event);
+    }
+}
+impl Dispatch<wl_registry::WlRegistry, ()> for App {
+    fn event(
+        _state: &mut Self,
+        _registry: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        _: &(),
+        _: &Connection,
+        _qh: &QueueHandle<App>,
+    ) {
+        println!("Got registry event {:?}",event);
+    }
+}
+impl Dispatch<XdgWmBase, ()> for App {
+    fn event(_state: &mut Self, proxy: &XdgWmBase, event: <XdgWmBase as Proxy>::Event, _data: &(), _conn: &Connection, _qhandle: &QueueHandle<Self>) {
+        match event {
+            wayland_protocols::xdg::shell::client::xdg_wm_base::Event::Ping {serial}  => {
+                proxy.pong(serial)
+            }
+            _ => {
+                println!("Unknown XdgWmBase event: {:?}", event); // Add this line
+
+            }
+        }
+    }
+}
+
+impl Dispatch<WlCompositor, ()> for App {
+    fn event(_state: &mut Self, _proxy: &WlCompositor, event: <WlCompositor as Proxy>::Event, _data: &(), _conn: &Connection, _qhandle: &QueueHandle<Self>) {
+        println!("Got compositor event {:?}",event);
+    }
+}
+
+impl Dispatch<WlShm, ()> for App {
+    fn event(_state: &mut Self, _proxy: &WlShm, event: <WlShm as Proxy>::Event, _data: &(), _conn: &Connection, _qhandle: &QueueHandle<Self>) {
+        println!("Got shm event {:?}",event);
+    }
+}
+impl Dispatch<WlSurface, ()> for App {
+    fn event(_state: &mut Self, _proxy: &WlSurface, event: <WlSurface as Proxy>::Event, _data: &(), _conn: &Connection, _qhandle: &QueueHandle<Self>) {
+        println!("got WlSurface event {:?}",event);
+    }
+}
+impl Dispatch<XdgSurface, ()> for App {
+    fn event(_state: &mut Self, _proxy: &XdgSurface, event: <XdgSurface as Proxy>::Event, _data: &(), _conn: &Connection, _qhandle: &QueueHandle<Self>) {
+        println!("got XdgSurface event {:?}",event);
+    }
+}
+impl Dispatch<XdgToplevel, ()> for App {
+    fn event(_state: &mut Self, _proxy: &XdgToplevel, event: <XdgToplevel as Proxy>::Event, _data: &(), _conn: &Connection, _qhandle: &QueueHandle<Self>) {
+        println!("got XdgToplevel event {:?}",event);
+        // match event {
+        //     xdg_toplevel::Event::Configure {  width, height, states: _ } => {
+        //         xdg_toplevel_configure_event(width, height);
+        //     }
+        //     _ => {
+        //         println!("got XdgToplevel event {:?}",event);
+        //
+        //     }
+        // }
+    }
+}
+impl Dispatch<WlShmPool, ()> for App {
+    fn event(_state: &mut Self, _proxy: &WlShmPool, event: <WlShmPool as Proxy>::Event, _data: &(), _conn: &Connection, _qhandle: &QueueHandle<Self>) {
+        println!("got WlShmPool event {:?}",event);
+    }
+}
+impl Dispatch<WlBuffer, ()> for App {
+    fn event(_state: &mut Self, _proxy: &WlBuffer, event: <WlBuffer as Proxy>::Event, _data: &(), _conn: &Connection, _qhandle: &QueueHandle<Self>) {
+        println!("got WlBuffer event {:?}",event);
+    }
+}
+
+
 impl Window {
-    pub async fn new(_position: Position, _size: Size, _title: String) -> Self {
-        todo!()
+    pub async fn new(position: Position, size: Size, title: String) -> Self {
+        let connection = Connection::connect_to_env().expect("Failed to connect to wayland server");
+        let display = connection.display();
+        let (globals, mut event_queue) = registry_queue_init::<App>(&connection).expect("Can't initialize registry");
+        let qh = event_queue.handle();
+        let xdg_wm_base: XdgWmBase = globals.bind(&qh, 6..=6, ()).unwrap();
+        let compositor: wl_compositor::WlCompositor = globals.bind(&qh, 6..=6, ()).unwrap();
+        let shm: WlShm = globals.bind(&qh, 2..=2, ()).unwrap();
+        let surface = compositor.create_surface(&qh, ());
+        // Create a toplevel surface
+        let xdg_surface = xdg_wm_base.get_xdg_surface(&surface, &qh, ());
+        xdg_surface.get_toplevel(&qh, ());
+
+        let (file, mmap) = create_shm_buffer(&shm, 200, 200);
+        let pool = shm.create_pool(file.as_fd(), mmap.len() as i32, &qh, ());
+        let buffer = pool.create_buffer(
+            0,
+            200,
+            200,
+            200 * 4,
+            Format::Argb8888,
+            &qh,
+            (),
+        );
+        surface.attach(Some(&buffer), 0, 0);
+        surface.commit();
+
+        // let seat: WlSeat = globals.bind(&qh, 8..=9, ()).expect("Can't bind seat");
+        // let _pointer = seat.get_pointer(&qh, surface.id());
+        // let _keyboard = seat.get_keyboard(&qh, surface.id());
+
+
+        println!("Window should be displayed. Running event loop...");
+
+        todo!();
+        // loop {
+        //     event_queue.blocking_dispatch(&mut app_data).unwrap();
+        // }
     }
 
     pub async fn default() -> Self {
-        todo!()
+        Window::new(Position::new(0.0, 0.0), Size::new(800.0, 600.0), "app_window".to_string()).await
     }
 
     pub async fn fullscreen(_title: String) -> Result<Self, FullscreenError> {
