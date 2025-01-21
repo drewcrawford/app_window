4bdf7e7d76b379e7c7833b2515b179957a34ed21
Drew Crawford <drew@sealedabstract.com>
2025-01-20 20:09:45 -0600

    Resize for ax

--- src/sys/linux.rs
+++ src/sys/linux.rs
@@ -43,6 +43,50 @@ mod ax {
     use libc::{access, close};
     use crate::coordinates::Size;
     use crate::sys::linux::{BUTTON_WIDTH, TITLEBAR_HEIGHT};
+    pub fn build_tree_update(title: String, window_size: Size) -> TreeUpdate {
+        let mut window = accesskit::Node::new(Role::Window);
+        window.set_label(title);
+        //accesskit rect is min and max, not origin and height!
+        window.set_bounds(Rect::new(0.0, 0.0, window_size.width(), window_size.height()));
+        let mut title_bar = accesskit::Node::new(Role::TitleBar);
+        title_bar.set_label("app_window");
+        title_bar.set_bounds(Rect::new(0.0, 0.0, window_size.width(), TITLEBAR_HEIGHT as f64));
+        let mut close_button = accesskit::Node::new(Role::Button);
+        close_button.add_action(Action::Click);
+        close_button.add_action(Action::Focus);
+
+        close_button.set_bounds(Rect::new(window_size.width() - BUTTON_WIDTH as f64, 0.0, window_size.width() as f64, TITLEBAR_HEIGHT as f64));
+        close_button.set_label("Close");
+
+        let mut maximize_button = accesskit::Node::new(Role::Button);
+        maximize_button.add_action(Action::Click);
+        maximize_button.add_action(Action::Focus);
+        maximize_button.set_bounds(Rect::new(window_size.width() - BUTTON_WIDTH as f64 * 2.0, 0.0, window_size.width() as f64 - BUTTON_WIDTH as f64 * 1.0, TITLEBAR_HEIGHT as f64));
+        maximize_button.set_label("Maximize");
+
+        let mut minimize_button = accesskit::Node::new(Role::Button);
+        minimize_button.add_action(Action::Click);
+        minimize_button.add_action(Action::Focus);
+        minimize_button.set_bounds(Rect::new(window_size.width() - BUTTON_WIDTH as f64 * 3.0, 0.0, window_size.width() as f64 - BUTTON_WIDTH as f64 * 2.0, TITLEBAR_HEIGHT as f64));
+        minimize_button.set_label("Minimize");
+
+        //window.set_children(vec![NodeId(2)]);
+        //title_bar.set_children(vec![NodeId(3),NodeId(4), NodeId(5)]);
+        window.set_children(vec![NodeId(3), NodeId(4), NodeId(5)]);
+
+        let tree = accesskit::Tree {
+            root: NodeId(1),
+            app_name: Some("app_window".to_string()),
+            toolkit_name: Some("app_window".to_string()),
+            toolkit_version: Some("0.1.0".to_string()),
+        };
+        let update = accesskit::TreeUpdate {
+            nodes: vec![(NodeId(1),window), /*(NodeId(2), title_bar),*/ (NodeId(3), close_button), (NodeId(4), maximize_button), (NodeId(5), minimize_button)],
+            tree: Some(tree),
+            focus: NodeId(3),
+        };
+        update
+    }
 
     pub struct Inner {
         window_size: Size,
@@ -61,49 +105,8 @@ mod ax {
     }
     impl accesskit::ActivationHandler for AX {
         fn request_initial_tree(&mut self) -> Option<TreeUpdate> {
-            let mut window = accesskit::Node::new(Role::Window);
-            window.set_label(self.inner.title.clone());
-            let window_size = self.inner.window_size;
-            //accesskit rect is min and max, not origin and height!
-            window.set_bounds(Rect::new(0.0, 0.0, window_size.width(), window_size.height()));
-            let mut title_bar = accesskit::Node::new(Role::TitleBar);
-            title_bar.set_label("app_window");
-            title_bar.set_bounds(Rect::new(0.0, 0.0, window_size.width(), TITLEBAR_HEIGHT as f64));
-            let mut close_button = accesskit::Node::new(Role::Button);
-            close_button.add_action(Action::Click);
-            close_button.add_action(Action::Focus);
 
-            close_button.set_bounds(Rect::new(window_size.width() - BUTTON_WIDTH as f64, 0.0, window_size.width() as f64, TITLEBAR_HEIGHT as f64));
-            close_button.set_label("Close");
-
-            let mut maximize_button = accesskit::Node::new(Role::Button);
-            maximize_button.add_action(Action::Click);
-            maximize_button.add_action(Action::Focus);
-            maximize_button.set_bounds(Rect::new(window_size.width() - BUTTON_WIDTH as f64 * 2.0, 0.0, window_size.width() as f64 - BUTTON_WIDTH as f64 * 1.0, TITLEBAR_HEIGHT as f64));
-            maximize_button.set_label("Maximize");
-
-            let mut minimize_button = accesskit::Node::new(Role::Button);
-            minimize_button.add_action(Action::Click);
-            minimize_button.add_action(Action::Focus);
-            minimize_button.set_bounds(Rect::new(window_size.width() - BUTTON_WIDTH as f64 * 3.0, 0.0, window_size.width() as f64 - BUTTON_WIDTH as f64 * 2.0, TITLEBAR_HEIGHT as f64));
-            minimize_button.set_label("Minimize");
-
-            //window.set_children(vec![NodeId(2)]);
-            //title_bar.set_children(vec![NodeId(3),NodeId(4), NodeId(5)]);
-            window.set_children(vec![NodeId(3), NodeId(4), NodeId(5)]);
-
-            let tree = accesskit::Tree {
-                root: NodeId(1),
-                app_name: Some("app_window".to_string()),
-                toolkit_name: Some("app_window".to_string()),
-                toolkit_version: Some("0.1.0".to_string()),
-            };
-            let mut update = accesskit::TreeUpdate {
-                nodes: vec![(NodeId(1),window), /*(NodeId(2), title_bar),*/ (NodeId(3), close_button), (NodeId(4), maximize_button), (NodeId(5), minimize_button)],
-                tree: Some(tree),
-                focus: NodeId(3),
-            };
-            Some(update)
+            Some(build_tree_update(self.inner.title.clone(), self.inner.window_size))
         }
     }
     impl accesskit::ActionHandler for AX {
@@ -331,6 +334,7 @@ struct WindowInternal {
     ax: Option<ax::AX>,
     size_update_notify: Option<Box<dyn Fn(Size) + Send>>,
     decor_subsurface: Option<WlSubsurface>,
+    title: String
 }
 impl WindowInternal {
 
@@ -347,8 +351,9 @@ impl WindowInternal {
             adapter = None;
             ax_impl = None;
         }
-        let ax = ax::AX::new(size, title);
+        let ax = ax::AX::new(size, title.clone());
         let mut window_internal = Arc::new(Mutex::new(WindowInternal{
+            title,
             app_state: Arc::downgrade(app_state),
             proposed_configure: None,
             //in case we are asked for size prior to configure?
@@ -730,6 +735,11 @@ impl Dispatch<XdgSurface, Arc<Mutex<WindowInternal>>> for App {
                             surface.commit();
                             locked_data.buffer = Some(buffer);
                         }
+                        let title = locked_data.title.clone();
+                        let applied_size = locked_data.applied_size();
+                        locked_data.adapter.as_mut().map(|a| a.update_if_active(|| {
+                            ax::build_tree_update(title, applied_size)
+                        }));
                         locked_data.size_update_notify.as_ref().map(|f| f(locked_data.applied_size()));
                     }
                 }
