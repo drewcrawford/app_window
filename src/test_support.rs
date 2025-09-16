ad100de034734fc9a928bfa4da13beba595fd788
Drew Crawford <drew@sealedabstract.com>
2025-09-15 19:22:38 -0500

    Fix clippy

--- src/test_support.rs
+++ src/test_support.rs
@@ -30,6 +30,7 @@ harness = false
 
 Then in `tests/your_custom_test_name.rs`:
 ```rust,no_run
+#[allow(clippy::needless_doctest_main)]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
 fn main() {
     app_window::test_support::integration_test_harness(|| {
