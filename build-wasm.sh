bcfd48e94cafe4103517a0a5c952f1fc5d942665
Drew Crawford <drew@sealedabstract.com>
2025-11-26 18:54:36 -0600

    updates

--- build-wasm.sh
+++
@@ -1,5 +0,0 @@
-CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUNNER="wasm-server-runner" \
-RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
-cargo +nightly run --example gpu --target wasm32-unknown-unknown \
---features wgpu \
--Z build-std=std,panic_abort
---
+++ examples/alert.rs
@@ -0,0 +1,27 @@
+/*!
+An example that displays an alert with "Hello World".
+*/
+use some_executor::SomeExecutor;
+use some_executor::observer::Observer;
+use some_executor::task::{Configuration, Task};
+
+pub fn main() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+    app_window::application::main(|| {
+        let task = Task::without_notifications(
+            "alert".to_string(),
+            Configuration::default(),
+            async {
+                app_window::alert("Hello World".to_string()).await;
+            },
+        );
+        some_executor::current_executor::current_executor()
+            .spawn_objsafe(task.into_objsafe())
+            .detach();
+        #[cfg(target_arch = "wasm32")]
+        wasm_bindgen::throw_str(
+            "Cursed hack to keep workers alive. See https://github.com/rustwasm/wasm-bindgen/issues/2945",
+        );
+    });
+}
